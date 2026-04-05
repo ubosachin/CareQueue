bdb3fd70e0488d56010074ba6fc672630502040d
Sachin Yadav <ubosachin@gmail.com>
2026-04-05 14:28:55 +0530

    dddd

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/queue/:path*", "/analytics/:path*", "/insights/:path*", "/schedule/:path*", "/settings/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/queue/:path*",
+    "/analytics/:path*",
+    "/insights/:path*",
+    "/schedule/:path*",
+    "/settings/:path*"
+  ],
 };
